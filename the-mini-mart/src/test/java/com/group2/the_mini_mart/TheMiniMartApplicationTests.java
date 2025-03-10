36440cb95a49b565ac617d131692cd704394a5f9
Gabriel Cheong <gabriel.cheong@screeningeagle.com>
2025-03-10 12:02:34 +0800

    Project setup

---
+++ the-mini-mart/src/test/java/com/group2/the_mini_mart/TheMiniMartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group2.the_mini_mart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheMiniMartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
